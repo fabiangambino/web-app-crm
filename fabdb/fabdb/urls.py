a25b98770231759c7c97977222b31f9ea2a53078
Fabian <fgambinoC@gmail.com>
2019-09-16 22:15:55 -0400

    removed unnecessary urls and made login.html the default view

--- fabdb/fabdb/urls.py
+++ fabdb/fabdb/urls.py
@@ -6,12 +6,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^profiles/', include('contacts.urls')),
-    url(r'^about/', views.about),
-    url(r'^login/', views.login),
     url(r'^results/', views.results_list),
     url(r'^dashboard/', views.dashboard),
     url(r'^search/', views.advanded_search),
-    url(r'^$', views.homepage),
+    url(r'^$', views.login),
 ]
 
 urlpatterns += staticfiles_urlpatterns()

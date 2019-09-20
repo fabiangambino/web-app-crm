dc5ff6131c27838bd2b3a4a6f714cce967b81b16
Fabian <fgambinoC@gmail.com>
2019-09-20 00:22:37 -0400

    finalized and tested contact models and organized them in an html table

--- fabdb/contacts/views.py
+++ fabdb/contacts/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Contact
 
 def all_contacts(request):
-    return render(request, 'contacts/all-contacts.html')
+    contacts = Contact.objects.all().order_by('date_created')
+    return render(request, 'contacts/all-contacts.html', {'contacts': contacts})
 
 def contact_view(request):
     return render(request, 'contacts/contact-view.html')
